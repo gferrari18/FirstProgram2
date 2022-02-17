1d9930738883cd6d6ce6d7966e74a10a3e4d16e1
gferrari18 <gferrari18@my.whitworth.edu>
2022-02-16 21:28:19 -0800

    Update

---
+++ basketball.py
@@ -0,0 +1,17 @@
+game1 = 13
+game2 = 18
+game3 = 21
+game4 = 17
+game5 = 31
+PointTotal = game1+game2+game3+game4+game5
+
+print("game 1 = " + str(game1))
+print("game 2 = " + str(game2))
+print("game 3 = " + str(game3))
+print("game 4 = " + str(game4))
+print("game 5 = " + str(game5))
+
+print("Total of Points = " + str(PointTotal))
+
+average = PointTotal/5
+print("Average of points = " + str(average))
---
+++ tip.py
@@ -0,0 +1,7 @@
+Bill = 47.56
+Total = Bill
+print("total = " + str(Total)) #This will print me the Bill total, with "total" before the actual value
+print("10% tip = " + str(Total*0.10)) #calculates tip based on bill value
+print("15% tip = " + str(Total*0.15)) 
+print("20% tip = " + str(Total*0.20)) 
+print("25% tip = " + str(Total*0.25))
