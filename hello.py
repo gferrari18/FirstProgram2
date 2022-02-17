f931f8a7cf36feb30a88d4b4ceb03e6692531702
gferrari18 <gferrari18@my.whitworth.edu>
2022-02-16 20:48:08 -0800

    First Program

---
+++ hello.py
@@ -0,0 +1,28 @@
+print("Hello World!")
+print("nice, I will learn a lot")
+print("someday I will be a good programmer")
+print("i will get good at it")
+print("someday, im sure")
+print("I will get a tech job")
+print("soon!")
+print("someday i will be saturday night")
+print("i do not know the rest of the lyrics")
+print("soon I will learn how to program cool softwares")
+print("nice")
+print("just like english and portuguese, Python is just another language")
+print("hopefully it will come easy. But if not, i will work hard to learn it")
+
+
+room = 1
+year = 2022
+
+print(room) #i hope you dont see this
+print(year)
+
+print(year + 1)
+
+year = year + 1
+
+print(year)
+
+print(year + 1)
