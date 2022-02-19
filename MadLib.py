b0d4b016bf8ed3c38feb8a44ab8fcac59e7c1936
gferrari18 <gferrari18@my.whitworth.edu>
2022-02-18 22:06:34 -0800

    nice

---
+++ MadLib.py
@@ -0,0 +1,9 @@
+
+print("Welcome to ya Boi's MadLib generator. Please provide me : ")
+
+adjective = input("An Adjective : ")
+noun1 = input("A noun : ")
+past_tense = input("A past tense verb : ")
+noun2 = input("...Another noun : ")
+
+print("The quick, " + adjective + " " +  noun1 + " " + past_tense + " over the lazy " + noun2 + ".")
