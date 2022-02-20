ce7d5d8bca7125fe8718157e33b53e729499e755
gferrari18 <gferrari18@my.whitworth.edu>
2022-02-19 17:09:14 -0800

    MyMadLib

---
+++ MyMadLib.py
@@ -0,0 +1,8 @@
+print("Hello. Welcome to my personal MadLib generator!")
+
+#You either die a hero or live long enough to see yourself become the villain...
+
+noun1 = input("Choose a noun: ")
+noun2 = input("Choose a different noun: ")
+
+print("You either die a " + noun1 + " or live long enough to see yourself become the " + noun2 + ".")
